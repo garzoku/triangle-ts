076f778cffc6c76da5ad53deed8f7e5891ac19bb
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-05-11 15:02:38 -0500

    make changes according to Master Jedi Kyle's wise instructions

--- triangle.ts
+++ triangle.ts
@@ -7,54 +7,46 @@ export class Triangle {
     this.side2 = side2;
     this.side3 = side3;
   }
+
+  get isValidTriangle() {
+    return (
+      this.side1 > 0 &&
+      this.side2 > 0 &&
+      this.side3 > 0 &&
+      this.side1 + this.side2 > this.side3 &&
+      this.side2 + this.side3 > this.side1 &&
+      this.side3 + this.side1 > this.side2
+    );
+  }
   private get isEquilateral() {
-    if (this.isTriangleInvalid()) {
-      return false;
-    } else if (this.side1 === this.side2 && this.side1 === this.side3) {
-      return true;
-    } else {
-      return false;
-    }
+    return (
+      this.isValidTriangle &&
+      this.side1 === this.side2 &&
+      this.side1 === this.side3
+    );
   }
   private get isIsosceles() {
-    if (this.isTriangleInvalid()) {
-      return false;
-    } else if (
-      (this.side1 === this.side2 && this.side1 === this.side3) ||
-      (this.side1 === this.side2 && this.side1 !== this.side3) ||
-      (this.side2 === this.side3 && this.side2 !== this.side1) ||
-      (this.side3 === this.side1 && this.side3 !== this.side2)
-    ) {
-      return true;
-    } else {
-      return false;
-    }
+    return (
+      (this.isValidTriangle &&
+        this.side1 === this.side2 &&
+        this.side1 === this.side3) ||
+      (this.isValidTriangle &&
+        this.side1 === this.side2 &&
+        this.side1 !== this.side3) ||
+      (this.isValidTriangle &&
+        this.side2 === this.side3 &&
+        this.side2 !== this.side1) ||
+      (this.isValidTriangle &&
+        this.side3 === this.side1 &&
+        this.side3 !== this.side2)
+    );
   }
   private get isScalene() {
-    if (this.isTriangleInvalid()) {
-      return false;
-    } else if (
+    return (
+      this.isValidTriangle &&
       this.side1 !== this.side2 &&
       this.side1 !== this.side3 &&
       this.side2 !== this.side3
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  isTriangleInvalid() {
-    if (
-      this.side1 === 0 ||
-      this.side2 === 0 ||
-      this.side3 === 0 ||
-      this.side1 + this.side2 <= this.side3 ||
-      this.side2 + this.side3 <= this.side1 ||
-      this.side3 + this.side1 <= this.side2
-    ) {
-      return true;
-    } else {
-      return false;
-    }
+    );
   }
 }
